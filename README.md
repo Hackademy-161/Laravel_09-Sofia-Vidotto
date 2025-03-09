2f2f366822e5e7975b650adb05a3f3d2492f935a
sofia vidotto <sofiavidotto8@gmail.com>
2025-03-09 12:17:12 +0100

    ReadMe ok - project init

---
+++ README.md
@@ -0,0 +1,24 @@
+## Project
+Create an online store for Korean cosmetics. The website should have a modern design and user-friendly interface that showcases the products in an attractive way. Additionally, the website should have a blog section that provides beauty tips and information about Korean skincare.
+
+## Palette
+- black #383838
+- blue-gray#697586
+- gray #8498AF
+- lilac  #B0A6BD
+- pink  #F5A3B7
+- light-pink  #FEE2E3
+- white #FFFFFF
+- off-white #F9F9FC 
+
+## Typography
+
+- Poppins 
+- Montserrat
+
+## Functional
+- Search
+- Shopping cart
+- Account -> Data change / Favorites / Orders
+- Product Review
+- Blog
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="/images/Bloom Beauty.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Shop All</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Bestsellers</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Collection</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li>
+            </ul>
+
+            <ul>
+                <li class="nav-item text-center mx-2 mx-lg-1">
+                    <a class="nav-link" href="#!">
+                        <div>
+                            <i class="fas fa-globe-americas fa-lg mb-1"></i>
+                            <span class="badge rounded-pill badge-notification bg-dark">11</span>
+                        </div>
+                        News
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
